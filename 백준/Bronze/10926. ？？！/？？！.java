6ac4ac9682a9c8a19426ec021d4bd34c9cc97c2d
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-02 21:30:06 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(str);
+        bw.write("??!");
+        bw.flush();
+        bw.close();
+
+    }
+}
