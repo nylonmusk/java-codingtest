dc108c9451b8afc21927e8fd995584cdbd46ce72
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-04 00:18:06 +0900

    [level 1] Title: 폰켓몬, Time: 4.21 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        if(set.size() >= nums.length / 2) {
+            answer = nums.length / 2;
+        }
+        if(set.size() < nums.length / 2) {
+            answer = set.size();
+        }
+        
+        return answer;
+    }
+}
