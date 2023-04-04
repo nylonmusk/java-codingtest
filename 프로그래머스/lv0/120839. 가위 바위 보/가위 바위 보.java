eb89fabb0ceef3991bdf0552689d6b205d787a3c
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-04 23:02:45 +0900

    [level 0] Title: 가위 바위 보, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuffer answer = new StringBuffer();
+        for(int i = 0; i < rsp.length(); i++) {
+            if(rsp.charAt(i) == ('2')) answer.append("0");
+            if(rsp.charAt(i) == ('0')) answer.append("5");
+            if(rsp.charAt(i) == ('5')) answer.append("2");
+        }
+        return answer.toString();
+    }
+}
