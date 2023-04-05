7f1c1eb295b9d8ac78f65d57c4dd55ff7497d3e6
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-05 22:57:22 +0900

    [level 0] Title: 가까운 수, Time: 0.42 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -3,22 +3,16 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] array, int n) {
         Arrays.sort(array);
-        // int leftValue = 0;
-        // int rightValue = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] > n) {
-                // leftValue = n - array[(i - 1)];
-                // rightValue = array[i] - n;
-                // return leftValue <= rightValue ? array[(i - 1)] : array[i];
                 if(i != 0){
-                if (n - array[(i - 1)] <= array[i] - n) {
-                    return array[(i - 1)];
-                }else {
-                    return array[i];
+                    if (n - array[(i - 1)] <= array[i] - n) {
+                        return array[(i - 1)];
+                    }else {
+                        return array[i];
+                    }
                 }
-                // return n - array[(i - 1)] <= array[i] - n ? array[(i - 1)] : array[i];
             }
-                }
         }
         return array[array.length - 1];
     }
