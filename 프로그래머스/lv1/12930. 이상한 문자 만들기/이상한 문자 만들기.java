223824ea07c7b5b44672006b42d3cc5f1bf471ca
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-09 14:47:59 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 7.95 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        int cnt = 0;
+        for(String ss : arr){
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        }
+        return answer;
+    }
+}
