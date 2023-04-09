9efa401ba1af151e29e8cfb08dcef4eb930ab3dc
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-09 14:45:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+  public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) return s.matches("(^[0-9]*$)");
+        return false;
+  }
+}
