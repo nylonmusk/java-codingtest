b290519f59f060a6eebbf926f5a622758e877924
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-09 14:48:30 +0900

    [level 1] Title: 예산, Time: 0.35 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int n : d) {
+            if (budget - n >= 0) {
+                answer++;
+                budget -= n;
+            } else {
+                break;
+            }
+        }
+
+
+        return answer;
+    }
+}
