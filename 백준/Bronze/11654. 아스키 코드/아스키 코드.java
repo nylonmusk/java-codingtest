04854ea6b02be59849c41c7c9fd6a6914f39b1da
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-11 01:58:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        char c = s.charAt(0);
+        int i = (int) c;
+
+        bw.write(Integer.toString(i));
+        bw.flush();
+        bw.close();
+    }
+}
