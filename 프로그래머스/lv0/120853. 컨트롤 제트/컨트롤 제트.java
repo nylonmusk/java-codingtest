a8fb5d96486d41df6f4bf685f4cc784bfe22a5e4
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-17 16:43:10 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.37 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,10 +1,32 @@
+// class Solution {
+//     public int solution(String s) {
+//         int answer = 0;
+//         String[] str = s.split(" ");
+//         for (int i = 0; i < str.length; i++) {
+//             answer += !str[i].equals("Z") ? Integer.parseInt(str[i]) : - Integer.parseInt(str[i-1]);  
+//         }
+//         return answer;
+//     }
+// }
+
+import java.util.Stack;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String[] str = s.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            answer += !str[i].equals("Z") ? Integer.parseInt(str[i]) : - Integer.parseInt(str[i-1]);  
+        Stack<String> st = new Stack<>();  
+        for (String str : s.split(" ")) {
+            st.push(str);
+            if(st.peek().equals("Z")) {
+                st.pop();
+                answer -= Integer.parseInt(st.peek());
+            } else {
+                answer += Integer.parseInt(st.peek());
+            }
         }
         return answer;
     }
 }
+
+
+
