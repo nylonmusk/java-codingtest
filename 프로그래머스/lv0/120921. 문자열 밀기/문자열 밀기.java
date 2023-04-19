d80e6db33b76b4ec81ff67e6807cf4b7bfe92ec5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-19 09:31:48 +0900

    [level 0] Title: 문자열 밀기, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,5 +1,17 @@
 class Solution {
     public int solution(String A, String B) {
-        return (B+B).indexOf(A);
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        sb.append(A);
+        int length = A.length();
+        for (int i = 0; i < length; i++) {
+            if (sb.toString().equals(B)) {
+                return answer;
+            }
+            sb.insert(0, sb.charAt(length - 1));
+            sb.deleteCharAt(length);
+            answer++;
+        }    
+        return -1;
     }
 }
