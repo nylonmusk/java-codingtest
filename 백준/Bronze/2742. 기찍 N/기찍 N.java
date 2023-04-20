2b20261d78d1006ee2600fadf13398e3ea546b78
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-20 18:21:09 +0900

    [Bronze IV] Title: 기찍 N, Time: 1416 ms, Memory: 31460 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = N; 0 < i; i--) {
+            System.out.println(i);
+        }
+    }
+} 
