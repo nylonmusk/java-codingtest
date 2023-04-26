25cf4828fa730c99015f2a15f30c8c5524e488dc
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-04-26 09:18:46 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] check = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!check[i]) {
+                dfs(computers, check, i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public void dfs (int[][] computers, boolean[] check, int start) {
+        check[start] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[start][i] == 1 && !check[i]) {
+                dfs(computers, check, i);
+            }    
+        }
+    }
+    
+    
+}
