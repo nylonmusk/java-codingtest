146afe7da356b25ad19ce22152a2b1e9f337adc9
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-05-02 20:49:12 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 84 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        bw.write(String.valueOf(n.divide(m)));
+        bw.newLine();
+        bw.write(String.valueOf(n.remainder(m)));
+        bw.flush();
+        bw.close();
+    }
+}
