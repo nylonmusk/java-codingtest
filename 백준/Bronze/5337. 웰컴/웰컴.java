7ba957da1098a9af8f9bb19ea370c27578e7e6b8
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-05-26 00:03:44 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .").append("\n").append("|  | _ | _. _ ._ _  _").append("\n").append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
