9bf9dc594db7d5fa12aaec3d0167c390d9bfdd4d
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-05-31 14:49:11 +0900

    [Bronze V] Title: Helicopter, Time: 56 ms, Memory: 5344 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,12 @@
+Module Module1
+    Sub Main()
+        Console.WriteLine("      ===================")
+        Console.WriteLine("          ____||___")
+        Console.WriteLine("\ /      /       []\")
+        Console.WriteLine(" X=======           \__")
+        Console.WriteLine("/ \      \____________|")
+        Console.WriteLine("            ||  ||")
+        Console.WriteLine("         \-----------/")
+        Console.ReadLine()
+    End Sub
+End Module
