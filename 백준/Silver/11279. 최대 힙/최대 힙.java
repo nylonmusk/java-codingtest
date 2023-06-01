312fab3fe18509c4c2964d878527174f3c6fe2a7
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-01 14:10:08 +0900

    [Silver II] Title: 최대 힙, Time: 332 ms, Memory: 27112 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        while (N-- > 0) {
+            int i = Integer.parseInt(br.readLine());
+            if (i == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.add(i);
+            }
+        }
+        System.out.println(sb);
+    }
+}
