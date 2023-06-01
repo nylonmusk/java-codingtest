90cb0d1bea0e95faa35d938090a701359ae35cdc
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-01 18:11:13 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int T = N;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < T; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+            T--;
+        }
+        System.out.println(sb);
+    }
+}
