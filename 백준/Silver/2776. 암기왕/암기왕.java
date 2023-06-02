6f69fc7b6ab3e6bd8bc144632e49a02b7dbe9ac9
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-02 09:42:55 +0900

    [Silver IV] Title: 암기왕, Time: 1736 ms, Memory: 288092 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            Set<Integer> set = new HashSet<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                set.add(Integer.parseInt(st.nextToken()));
+            }
+            int M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                int temp = Integer.parseInt(st.nextToken());
+                if (set.contains(temp)) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
