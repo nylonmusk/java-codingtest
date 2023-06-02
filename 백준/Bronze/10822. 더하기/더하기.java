98474bafee7e8d95343752dfd3552b95401d6eb3
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-02 15:21:22 +0900

    [Bronze II] Title: 더하기, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ss = br.readLine();
+        int count = 0;
+        for (String s : ss.split(",")) {
+            count += Integer.parseInt(s);
+        }
+        System.out.println(count);
+    }
+}
