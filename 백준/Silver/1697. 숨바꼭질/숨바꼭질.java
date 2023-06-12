761fb365b13deca8b8a780635e4d5de24fb42f1d
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-12 17:36:57 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        visited = new boolean[100001];
+        System.out.println(bfs(N, K));
+    }
+
+    private static int bfs(int N, int K) {
+        int count = 0;
+        visited[N] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int current = queue.poll();
+                
+                if (current == K) {
+                    return count;
+                }
+
+                if (current - 1 >= 0 && !visited[current - 1]) {
+                    visited[current - 1] = true;
+                    queue.offer(current - 1);
+                }
+
+                if (current + 1 <= 100000 && !visited[current + 1]) {
+                    visited[current + 1] = true;
+                    queue.offer(current + 1);
+                }
+
+                if (current * 2 <= 100000 && !visited[current * 2]) {
+                    visited[current * 2] = true;
+                    queue.offer(current * 2);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+}
