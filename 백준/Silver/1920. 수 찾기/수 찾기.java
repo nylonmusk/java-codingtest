eba043121eeed75895a64fcee1b8441df203bf7b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-15 10:30:08 +0900

    [Silver IV] Title: 수 찾기, Time: 568 ms, Memory: 48432 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,31 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arrN = new int[N];
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> setN = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            arrN[i] = sc.nextInt();
+            setN.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(arrN);
-        int M = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int num = sc.nextInt();
-            if (binarySearch(arrN, num)) {
-                sb.append("1\n");
+            int value = Integer.parseInt(st.nextToken());
+            if (setN.contains(value)) {
+                answer.append(1);
             } else {
-                sb.append("0\n");
+                answer.append(0);
             }
+            answer.append("\n");
         }
-        System.out.print(sb);
-    }
-
-    public static boolean binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return true;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
+        System.out.println(answer);
     }
 }
