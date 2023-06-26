aeb86c558f9fbdbc6ae822e2d2feddd8d331b0ca
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-26 16:33:08 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N;
+    static int M;
+    static int[] sequence;
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sequence = new int[M];
+
+        recursion(1, 0);
+        System.out.println(answer);
+    }
+
+    private static void recursion(int start, int depth) {
+        if (depth == M) {
+            for (int i : sequence) {
+                answer.append(i).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            sequence[depth] = i;
+            recursion(i, depth + 1);
+        }
+    }
+}
