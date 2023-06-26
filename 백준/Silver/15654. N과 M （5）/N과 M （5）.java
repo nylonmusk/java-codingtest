a047474e70f3e80c23938bf23f51d503e9911e15
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-26 16:43:26 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 29236 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N;
+    static int M;
+    static int[] sequence;
+    static boolean[] visited;
+    static List<Integer> nums = new ArrayList<>();
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+        nums.sort((a, b) -> a - b);
+        sequence = new int[M];
+
+        recursion(nums.get(0), 0);
+        System.out.println(answer);
+    }
+
+    private static void recursion(int start, int depth) {
+        if (depth == M) {
+            for (int i : sequence) {
+                answer.append(i).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                sequence[depth] = nums.get(i);
+                recursion(i, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
