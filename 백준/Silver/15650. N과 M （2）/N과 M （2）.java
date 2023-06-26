0cf5437dda997cd28951b0ed58ddcef08825aa29
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-26 16:29:45 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] sequence;
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sequence = new int[M];
+        visited = new boolean[N];
+        
+        recursion(1, 0);
+        System.out.println(answer);
+    }
+
+    private static void recursion(int start, int depth) {
+        if (depth == M) {
+            for (int i : sequence) {
+                answer.append(i).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            if (!visited[i - 1]) {
+                visited[i - 1] = true;
+                sequence[depth] = i;
+                recursion(i + 1, depth + 1);
+                visited[i - 1] = false;
+            }
+        }
+    }
+}
