5fedc5b31cecd4d3214972ef386f9d4c76ff6016
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-06-30 16:39:54 +0900

    [Silver I] Title: 정수 삼각형, Time: 296 ms, Memory: 27292 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] triangle = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N + 1][N + 1];
+        dp[0][0] = triangle[0][0];
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = dp[i - 1][0] + triangle[i][0];
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+            }
+        }
+
+        int answer = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[N - 1][i]);
+        }
+        System.out.println(answer);
+    }
+}
