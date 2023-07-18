fe19d475e1f8f6cbc0f0937818a72ca92b2610b2
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-07-18 17:42:34 +0900

    [Silver III] Title: 모든 순열, Time: 2200 ms, Memory: 61200 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    static int N;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        visited = new boolean[N + 1];
+        permutation(0);
+    }
+
+    static void permutation(int depth) {
+        if (depth == N) {
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                permutation(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
