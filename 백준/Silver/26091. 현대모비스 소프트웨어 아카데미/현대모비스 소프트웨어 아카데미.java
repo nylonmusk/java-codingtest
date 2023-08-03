c245293835f18d5b97b13b3be719379bdb2c6f5f
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-08-03 17:47:52 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 468 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int start = 0;
+        int end = n - 1;
+        int answer = 0;
+        while (start < end) {
+            int value = arr[start] + arr[end];
+            if (value < target) {
+                start++;
+            } else {
+                answer++;
+                start++;
+                end--;
+            }
+        }
+        System.out.println(answer);
+    }
+}
