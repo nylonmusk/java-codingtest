75a6a0692b3ab093ceaf75fc5fecfcc132181387
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-08-22 15:34:25 +0900

    [Gold V] Title: 센서, Time: 196 ms, Memory: 16356 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+
+        int[] sensors = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sensors[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sensors);
+
+        int[] distances = new int[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            distances[i] = sensors[i + 1] - sensors[i];
+        }
+        Arrays.sort(distances);
+
+        int answer = 0;
+        for (int i = 0; i < N - K; i++) {
+            answer += distances[i];
+        }
+
+        System.out.println(answer);
+    }
+}
