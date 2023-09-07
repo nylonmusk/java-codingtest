cbcb40f78ab30ed9a186d9866a7188e8da16375b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-09-07 11:59:57 +0900

    [Silver V] Title: 스네이크버드, Time: 144 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        List<Integer> numbers = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        numbers.sort((a, b) -> a - b);
+
+        for (int num : numbers) {
+            if (num <= L) L++;
+        }
+
+        System.out.println(L);
+    }
+}
