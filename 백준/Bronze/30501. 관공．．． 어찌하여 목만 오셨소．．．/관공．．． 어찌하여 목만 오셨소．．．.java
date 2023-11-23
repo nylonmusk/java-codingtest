cb23bc141d103b3062b44f6ab742c989d07f19f5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-11-23 10:15:25 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 132 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            String s = br.readLine();
+            if (s.contains("S")) {
+                System.out.println(s);
+            }
+        }
+    }
+}
