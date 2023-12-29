b9cae24e7a94378819ff905bc29be4a84abe563a
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2023-12-29 11:33:55 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        if (s.equals("fdsajkl;") | s.equals("jkl;fdsa")) {
+            System.out.println("in-out");
+        } else if (s.equals("asdf;lkj") | s.equals(";lkjasdf")) {
+            System.out.println("out-in");
+        } else if (s.equals("asdfjkl;")) {
+            System.out.println("stairs");
+        } else if (s.equals(";lkjfdsa")){
+            System.out.println("reverse");
+        } else {
+            System.out.println("molu");
+        }
+
+    }
+}
