8fde6ed0ef4fd3e3441c8eec6fd67ce2859a4b7d
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-01-15 16:10:45 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,94 +1,76 @@
-import java.io.BufferedReader;
+ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static String[][] rgb;
-    static boolean[][] visited;
-    static int N;
+    private static int N;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
+    private static char[][] colors;
+    private static boolean[][] visited;
+    private static boolean[][] visitedColorBlindness;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(st.nextToken());
-        visited = new boolean[N + 1][N + 1];
-        rgb = new String[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            String temp = br.readLine();
-            for (int j = 1; j <= N; j++) {
-                char c = temp.charAt(j - 1);
-                if (c == 'R') {
-                    rgb[i][j] = "R";
-                } else if (c == 'G') {
-                    rgb[i][j] = "G";
-                } else {
-                    rgb[i][j] = "B";
-                }
+
+        N = Integer.parseInt(br.readLine());
+        colors = new char[N][N];
+        visited = new boolean[N][N];
+        visitedColorBlindness = new boolean[N][N];
+
+        for (int j = 0; j < N; j++) {
+            int index = 0;
+            for (char c : br.readLine().toCharArray()) {
+                colors[j][index] = c;
+                index++;
             }
         }
 
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+        int countColorBlindness = 0;
+        int countNormal = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+
                 if (!visited[i][j]) {
-                    count++;
                     dfs(i, j);
+                    countNormal++;
+                }
+
+                if (!visitedColorBlindness[i][j]) {
+                    dfsColorBlindness(i, j);
+                    countColorBlindness++;
                 }
             }
         }
 
-        sb.append(count).append(" ");
-        visited = new boolean[N + 1][N + 1];
-        count = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (!visited[i][j]) {
-                    count++;
-                    dfsRGB(i, j);
-                }
-            }
-        }
-        sb.append(count);
-        System.out.println(sb);
+        System.out.println(countNormal + " " + countColorBlindness);
     }
 
-    public static void dfs(int x, int y) {
+    private static void dfs(int x, int y) {
         visited[x][y] = true;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx > 0 && ny > 0 && nx <= N && ny <= N && !visited[nx][ny] && rgb[nx][ny].equals(rgb[x][y])) {
-                visited[nx][ny] = true;
-                dfs(nx, ny);
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny]) {
+
+                if (colors[x][y] == colors[nx][ny]) {
+                    dfs(nx, ny);
+                }
             }
         }
     }
 
-    public static void dfsRGB(int x, int y) {
-        visited[x][y] = true;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
+    private static void dfsColorBlindness(int x, int y) {
+        visitedColorBlindness[x][y] = true;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx > 0 && ny > 0 && nx <= N && ny <= N && !visited[nx][ny]) {
-                if (rgb[x][y].equals("R") || rgb[x][y].equals("G")) {
-                    if (rgb[nx][ny].equals("R") || rgb[nx][ny].equals("G")) {
-                        dfsRGB(nx, ny);
-                    }
-                }
-                if (rgb[x][y].equals("B")) {
-                    if (rgb[nx][ny].equals(rgb[x][y])) {
-                        dfsRGB(nx, ny);
-                    }
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visitedColorBlindness[nx][ny]) {
+                if (colors[x][y] == colors[nx][ny] || colors[x][y] == 'R' && colors[nx][ny] == 'G' || colors[x][y] == 'G' && colors[nx][ny] == 'R') {
+                    visitedColorBlindness[nx][ny] = true;
+                    dfsColorBlindness(nx, ny);
                 }
             }
         }
