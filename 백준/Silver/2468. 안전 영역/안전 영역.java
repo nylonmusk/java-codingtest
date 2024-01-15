0cdb5027e757ad7cb69bbdf10df1cf330a1c1891
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-01-15 15:35:45 +0900

    [Silver I] Title: 안전 영역, Time: 228 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,65 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] locations;
-    static boolean[][] visited;
-    static int N;
-    static int C;
+    private static int N;
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
+    private static int[][] regions;
+    private static boolean[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        locations = new int[N][N];
+        regions = new int[N][N];
 
-        Set<Integer> set = new HashSet<>();
+        int maxHeight = Integer.MIN_VALUE;
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                locations[i][j] = Integer.parseInt(st.nextToken());
-                set.add(locations[i][j]);
+                int height = Integer.parseInt(st.nextToken());
+                regions[i][j] = height;
+                maxHeight = Math.max(maxHeight, height);
             }
         }
-        List<Integer> list = new ArrayList<>(set);
-        int max = Integer.MIN_VALUE;
-        for (int c = 0; c < list.size(); c++) {
-            C = list.get(c);
+
+        int height = 1;
+        int answer = 0;
+        while (height < maxHeight) {
             int count = 0;
             visited = new boolean[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (!visited[i][j] && locations[i][j] > C) {
-                        count += dfs(i, j);
+                    if (regions[i][j] > height && !visited[i][j]) {
+                        dfs(i, j, height);
+                        count++;
                     }
                 }
             }
-            max = Math.max(max, count);
+            answer = Math.max(answer, count);
+            height++;
         }
-        if (max == 0) {
-            System.out.println(1);
-        } else {
-            System.out.println(max);
-        }
+        System.out.println(answer == 0 ? 1 : answer);
     }
 
-    private static int dfs(int x, int y) {
-        visited[x][y] = true;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-
-        for (int i = 0; i < dx.length; i++) {
+    private static void dfs(int x, int y, int height) {
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny] && locations[nx][ny] > C) {
-                dfs(nx, ny);
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                if (!visited[nx][ny] && regions[nx][ny] > height) {
+                    visited[nx][ny] = true;
+                    dfs(nx, ny, height);
+                }
             }
         }
-        return 1;
     }
 }
