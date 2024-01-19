9fd2fb2c41addbfe1d8d92bb426147591e6b2e53
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-01-19 12:21:47 +0900

    [Silver II] Title: 마인크래프트, Time: 748 ms, Memory: 36628 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -4,24 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[n][m];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int[][] blocks = new int[N][M];
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                min = Math.min(min, board[i][j]);
-                max = Math.max(max, board[i][j]);
+            for (int j = 0; j < M; j++) {
+                blocks[i][j] = Integer.parseInt(st.nextToken());
+                min = Math.min(min, blocks[i][j]);
+                max = Math.max(max, blocks[i][j]);
             }
         }
 
@@ -29,22 +29,22 @@ public class Main {
         int height = 0;
 
         for (int i = min; i <= max; i++) {
-            int blocks = b;
+            int block = B;
             int time = 0;
 
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < m; k++) {
-                    if (board[j][k] > i) {
-                        time += (board[j][k] - i) * 2;
-                        blocks += (board[j][k] - i);
-                    } else if (board[j][k] < i) {
-                        time += (i - board[j][k]);
-                        blocks -= (i - board[j][k]);
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (blocks[j][k] > i) {
+                        time += (blocks[j][k] - i) * 2;
+                        block += (blocks[j][k] - i);
+                    } else if (blocks[j][k] < i) {
+                        time += (i - blocks[j][k]);
+                        block -= (i - blocks[j][k]);
                     }
                 }
             }
 
-            if (blocks >= 0 && minTime >= time) {
+            if (block >= 0 && minTime >= time) {
                 minTime = time;
                 height = i;
             }
