bf2242c95545472a8d2a5d65fb82922cc787afd8
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-01-25 10:55:06 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 756 ms, Memory: 37276 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,23 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+
         Map<String, Integer> words = new HashMap<>();
-        while (N-- > 0) {
+
+        for (int i = 0; i < N; i++) {
             String word = br.readLine();
             if (word.length() >= M) {
                 words.put(word, words.getOrDefault(word, 0) + 1);
@@ -26,26 +21,25 @@ class Main {
 
         List<String> keySet = new ArrayList<>(words.keySet());
 
-        Collections.sort(keySet, new Comparator<String>() {
-            @Override
-            public int compare(String word1, String word2) {
-                int frequency = Integer.compare(words.get(word2), words.get(word1));
-                if (frequency != 0) {
-                    return frequency;
-                }
-
-                int length = Integer.compare(word2.length(), word1.length());
-                if (length != 0) {
-                    return length;
-                }
-
-                return word1.compareTo(word2);
+        keySet.sort((word1, word2) -> {
+            int frequency = Integer.compare(words.get(word2), words.get(word1));
+            if (frequency != 0) {
+                return frequency;
             }
+
+            int length = Integer.compare(word2.length(), word1.length());
+            if (length != 0) {
+                return length;
+            }
+
+            return word1.compareTo(word2);
         });
 
+        StringBuilder answer = new StringBuilder();
         for (String s : keySet) {
             answer.append(s).append("\n");
         }
+        
         System.out.println(answer);
     }
 }
