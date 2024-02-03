ff53b58cbcabba7b858e7bf69b8fc0c94bc2abe5
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-02-03 15:29:59 +0900

    [Silver I] Title: RGB거리, Time: 124 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -24,6 +24,7 @@ public class Main {
             dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + houses[i][1];
             dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + houses[i][2];
         }
+        
         System.out.println(Math.min(dp[n - 1][0], Math.min(dp[n - 1][1], dp[n - 1][2])));
     }
 }
