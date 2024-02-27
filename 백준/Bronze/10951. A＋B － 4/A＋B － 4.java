8469009fd796d67fdf03066b7e7a658599823a9d
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-02-27 15:10:42 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,25 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder answer = new StringBuilder();
         StringTokenizer st;
-        String line;
-        while ((line = br.readLine()) != null) {
-            st = new StringTokenizer(line);
-            String a = st.nextToken();
-            String b = st.nextToken();
-            int sum = Integer.parseInt(a) + Integer.parseInt(b);
-            bw.write(Integer.toString(sum));
-            bw.newLine();
+        String s;
+        while ((s = br.readLine()) != null) {
+            st = new StringTokenizer(s);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            answer.append(A + B).append("\n");
         }
-        bw.flush();
-        bw.close();
+        
+        System.out.println(answer);
     }
 }
