8163712008b97a2a1f39b8fa934255180ae9a2eb
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-04-06 20:06:22 +0900

    [Bronze III] Title: 골뱅이 찍기 - 정사각형, Time: 284 ms, Memory: 19828 KB -BaekjoonHub

---
+++ 백준/Bronze/23794. 골뱅이 찍기 － 정사각형/골뱅이 찍기 － 정사각형.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n + 2; i++) {
+            System.out.print("@");
+        }
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("@");
+            for (int j = 0; j < n; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("@");
+        }
+
+        for (int i = 0; i < n + 2; i++) {
+            System.out.print("@");
+        }
+    }
+}
