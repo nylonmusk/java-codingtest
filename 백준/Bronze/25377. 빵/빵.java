00bb9f2908f5588ac5884149fc8fa8472e8c3dca
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-05-12 12:02:16 +0900

    [Bronze IV] Title: 빵, Time: 132 ms, Memory: 14312 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A <= B) {
+                answer = Math.min(answer, B);
+            }
+        }
+
+        System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+    }
+}
