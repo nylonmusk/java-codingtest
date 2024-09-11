39cbb71ab24bf467a2fe626e95d783d165d39d92
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-09-11 09:23:55 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print((int)Math.pow(2, Integer.parseInt(br.readLine())));
+    }
+}
