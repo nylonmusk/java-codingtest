fb0f638cfbecb2b34acb6ee6971fbb9e4e547366
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-09-13 14:45:55 +0900

    [Bronze I] Title: A/B - 2, Time: 124 ms, Memory: 15984 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.java
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -1,14 +1,25 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] input = br.readLine().split(" ");
-        double a = Double.parseDouble(input[0]);
-        double b = Double.parseDouble(input[1]);
-
-        System.out.println(a / b);
+        StringBuilder answer = new StringBuilder();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        answer.append(A / B + ".");
+        A = 10 * (A - B * (A / B));
+        int T = 1500;
+        while (T-- > 0) {
+            answer.append(A / B + "");
+            A = 10 * (A - B * (A / B));
+        }
+        System.out.println(answer);
     }
+
 }
