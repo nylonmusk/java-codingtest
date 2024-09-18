1e9cee396e8be3343c96ff323857f6b1fc1cab63
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-09-18 14:10:24 +0900

    [Bronze IV] Title: Oddities, Time: 112 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x % 2 == 0) {
+                System.out.println(x + " is even");
+            } else {
+                System.out.println(x + " is odd");
+            }
+        }
+    }
+}
