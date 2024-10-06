194c09a2f8fbf7212c182926eaec057e59be2266
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-10-06 16:49:22 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long count = (long) n * (n - 1) * (n - 2) / 6;
+
+        System.out.println(count);
+        System.out.println(3);
+    }
+}
