909557e6f76107dda3fc1859edba14bf1fba5dbe
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-12-08 14:15:33 +0900

    [Bronze III] Title: Building Pyramids, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/32585. Building Pyramids/Building Pyramids.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long result = (long) n * (n + 1) * (n + 2) / 6;
+        System.out.println(result);
+    }
+}
