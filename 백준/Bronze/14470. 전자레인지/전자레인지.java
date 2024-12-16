325e784e7528b3db96153f4444b12d9d3063272f
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-12-17 08:44:17 +0900

    [Bronze IV] Title: 전자레인지, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        int totalTime = 0;
+
+        if (A < 0) {
+            totalTime += Math.abs(A) * C;
+            totalTime += D;
+            totalTime += B * E;
+        } else if (A == 0) {
+            totalTime += D;
+            totalTime += B * E;
+        } else {
+            totalTime += (B - A) * E;
+        }
+
+        System.out.println(totalTime);
+    }
+}
