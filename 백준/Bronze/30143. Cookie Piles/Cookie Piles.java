9b2dccc137ad3ec75f4d1f2542c0025bcfeb325b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2024-12-20 09:44:40 +0900

    [Bronze IV] Title: Cookie Piles, Time: 112 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/30143. Cookie Piles/Cookie Piles.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int A = Integer.parseInt(st.nextToken());
+            int D = Integer.parseInt(st.nextToken());
+
+            System.out.println((N * (2 * A + (N - 1) * D)) / 2);
+        }
+    }
+}
