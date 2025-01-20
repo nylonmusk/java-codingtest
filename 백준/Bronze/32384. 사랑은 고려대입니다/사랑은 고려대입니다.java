4a54e9265e4c3402812d6133a249c41f00a9425e
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-01-21 08:45:03 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            if (i > 0) {
+                answer.append(" ");
+            }
+            answer.append("LoveisKoreaUniversity");
+        }
+
+        System.out.println(answer);
+    }
+}
