9215f0a21fb30750745e9c8ee08a019c0c1c0a07
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-02-09 22:00:48 +0900

    [Bronze V] Title: Rats, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n12 = Integer.parseInt(st.nextToken());
+
+        System.out.println((n1 + 1) * (n2 + 1) / (n12 + 1) - 1);
+    }
+}
