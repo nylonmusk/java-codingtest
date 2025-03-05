c81fc7a10db74fe9ec9d0b4ae58ff57081ff69fc
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-03-05 14:52:47 +0900

    [Bronze IV] Title: 短針 (Hour Hand), Time: 104 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/24083. 短針 （Hour Hand）/短針 （Hour Hand）.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        int answer = (A + B) % 12;
+        if (answer == 0) answer = 12;
+
+        System.out.println(answer);
+    }
+}
