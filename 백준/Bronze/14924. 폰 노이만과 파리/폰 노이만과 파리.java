a6e6523ecc17dbe2c6a538c3a2adf99bf044a138
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-04-09 11:04:37 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        
+        System.out.println(D * T / (2 * S));
+    }
+}
