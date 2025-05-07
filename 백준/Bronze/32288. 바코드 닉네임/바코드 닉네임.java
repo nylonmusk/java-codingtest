8a2862bd40341915b626434bf1768176d95ea64a
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-05-07 15:14:33 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 112 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
+            } else {
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
