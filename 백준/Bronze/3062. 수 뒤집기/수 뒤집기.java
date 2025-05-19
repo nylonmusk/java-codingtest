6880fc6a0b31ae95ffddfaaee2d5870054cb908f
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-05-19 15:35:14 +0900

    [Bronze II] Title: 수 뒤집기, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/3062. 수 뒤집기/수 뒤집기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int reversed = reverse(N);
+            int sum = N + reversed;
+
+            if (isPalindrome(sum)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    private static int reverse(int num) {
+        int reversed = 0;
+        while (num > 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        return reversed;
+    }
+
+    private static boolean isPalindrome(int num) {
+        String str = Integer.toString(num);
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
