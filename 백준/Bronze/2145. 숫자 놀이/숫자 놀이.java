0afe6c65b88eb4f38d056d79d8351b8c65e0477b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-06-01 12:41:58 +0900

    [Bronze II] Title: 숫자 놀이, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/2145. 숫자 놀이/숫자 놀이.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = br.readLine();
+            int n = Integer.parseInt(line);
+            if (n == 0) break;
+
+            System.out.println(digitSum(n));
+        }
+    }
+
+    private static int digitSum(int n) {
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n;
+    }
+}
