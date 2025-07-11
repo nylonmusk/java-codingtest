de73646f0d8a01bdba4aded29acc626ad6394f8b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-07-11 12:31:06 +0900

    [Bronze II] Title: 타임머신, Time: 100 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/1440. 타임머신/타임머신.java
+++ 백준/Bronze/1440. 타임머신/타임머신.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 
 public class Main {
 
-    static boolean isValid(int h, int m, int s) {
-        return (1 <= h && h <= 12) && (0 <= m && m <= 59) && (0 <= s && s <= 59);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] time = br.readLine().split(":");
@@ -27,5 +23,9 @@ public class Main {
         if (isValid(nums[2], nums[1], nums[0])) count++;
 
         System.out.println(count);
+    }
+    
+    private static boolean isValid(int h, int m, int s) {
+        return (1 <= h && h <= 12) && (0 <= m && m <= 59) && (0 <= s && s <= 59);
     }
 }
