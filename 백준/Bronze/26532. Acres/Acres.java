ca419e0bba4619acc29f645917167b5f1dc0c9b0
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-10-03 07:04:41 +0900

    [Bronze IV] Title: Acres, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long area = a * b;
+        long oneBag = 4840 * 5;
+
+        long result = (area + oneBag - 1) / oneBag;
+        System.out.println(result);
+    }
+}
