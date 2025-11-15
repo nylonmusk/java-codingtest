ce5a90ff4e43ba49be01cdfdef336862f797be6b
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-11-15 18:22:45 +0900

    [Silver IV] Title: 토너먼트, Time: 108 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int round = 0;
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+        System.out.println(round);
+    }
+}
