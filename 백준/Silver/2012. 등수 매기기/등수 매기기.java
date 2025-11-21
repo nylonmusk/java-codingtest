a72c21c383c6d4a1f5cab18d0aa86910defb53d9
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2025-11-21 19:05:08 +0900

    [Silver III] Title: 등수 매기기, Time: 676 ms, Memory: 55344 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += Math.abs(arr[i] - (i + 1));
+        }
+
+        System.out.println(sum);
+    }
+}
