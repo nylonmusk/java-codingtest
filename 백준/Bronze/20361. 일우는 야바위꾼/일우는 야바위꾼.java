3fdef4e40ef370b08d32614d76770f0824ce6a96
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2026-01-12 18:40:11 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 316 ms, Memory: 37780 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int pos = X;
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (pos == A) pos = B;
+            else if (pos == B) pos = A;
+        }
+
+        System.out.print(pos);
+    }
+}
